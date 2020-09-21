da09f063ae1c6d333d4f9976a74a49331843251b
Roee Shachar <shacharroy14@gmail.com>
2020-09-21 14:53:33 +0300

    feat: kafka message convention (#27)
    * feat: kafka message convention
    
    * fix: newline
    
    * chore: interface name changes

--- src/controllers/exportGeopackage.ts
+++ src/controllers/exportGeopackage.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import httpStatus from 'http-status-codes';
 import { delay, inject, injectable } from 'tsyringe';
 import { KafkaManager } from '../kafka/manager';
-import { IExportRequest } from '../model/exportRequest';
+import { IInboundRequest, IOutboundRequest } from '../model/exportRequest';
 
 @injectable()
 export class ExportGeopackageController {
@@ -11,16 +11,22 @@ export class ExportGeopackageController {
   }
 
   public async exportRequestHandler(req: Request, res: Response): Promise<Response> {
-    const requestBody = req.body as IExportRequest;
-    let message;
+    const requestBody = req.body as IInboundRequest;
+    let messageToSend : string;
     try {
-      message = JSON.stringify(requestBody);
+      const parsedMessage : IOutboundRequest = {
+        taskId: 'test',
+        filename: requestBody.fileName,
+        url: requestBody.exportedLayers[0].url,
+        bbox: requestBody.bbox
+      }
+      messageToSend = JSON.stringify(parsedMessage);
     } catch (err) {
       return res.status(httpStatus.BAD_REQUEST).json(err);
     }
 
     try {
-      await this.kafkaManager.sendMessage(message);
+      await this.kafkaManager.sendMessage(messageToSend);
       return res.status(httpStatus.OK).json({ success: true });
     } catch (err) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
--- src/model/exportRequest.ts
+++ src/model/exportRequest.ts
@@ -1,11 +1,18 @@
-export interface IExportRequest {
-  'fileName': string,
-  'directoryName': string,
-  'bbox': string,
-  'exportedLayers': ILayerData[]
+export interface IInboundRequest {
+  'fileName': string;
+  'directoryName': string;
+  'bbox': Array<number>;
+  'exportedLayers': ILayerData[];
 }
 
 export interface ILayerData {
-  'url': string,
-  'exportType': string
+  'url': string;
+  'exportType': string;
+}
+
+export interface IOutboundRequest {
+  'taskId': string;
+  'filename': string;
+  'url': string;
+  'bbox': Array<number>;
 }
