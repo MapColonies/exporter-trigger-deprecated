c2711f203dfbc1ee021b574c6f18936defd18a95
shimoncohen <shimoncohen655@gmail.com>
2020-10-14 16:04:03 +0300

    chore(uuid): generate uuid for taskId

--- src/controllers/exportGeopackage.ts
+++ src/controllers/exportGeopackage.ts
@@ -1,25 +1,31 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status-codes';
 import { delay, inject, injectable } from 'tsyringe';
+import { v4 as uuidv4 } from 'uuid';
 import { KafkaManager } from '../kafka/manager';
 import { IInboundRequest, IOutboundRequest } from '../model/exportRequest';
 
 @injectable()
 export class ExportGeopackageController {
+  public constructor(
+    @inject(delay(() => KafkaManager))
+    private readonly kafkaManager: KafkaManager
+  ) {}
 
-  public constructor(@inject(delay(() => KafkaManager)) private readonly kafkaManager: KafkaManager) {
-  }
-
-  public async exportRequestHandler(req: Request, res: Response): Promise<Response> {
+  public async exportRequestHandler(
+    req: Request,
+    res: Response
+  ): Promise<Response> {
     const requestBody = req.body as IInboundRequest;
-    let messageToSend : string;
+    const taskId = uuidv4();
+    let messageToSend: string;
     try {
-      const parsedMessage : IOutboundRequest = {
-        taskId: 'test',
-        filename: requestBody.fileName,
+      const parsedMessage: IOutboundRequest = {
+        taskId: taskId,
+        fileName: requestBody.fileName,
         url: requestBody.exportedLayers[0].url,
-        bbox: requestBody.bbox
-      }
+        bbox: requestBody.bbox,
+      };
       messageToSend = JSON.stringify(parsedMessage);
     } catch (err) {
       return res.status(httpStatus.BAD_REQUEST).json(err);
@@ -30,7 +36,10 @@ export class ExportGeopackageController {
       return res.status(httpStatus.OK).json({ success: true });
     } catch (err) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
-      const statusCode = err && err.meta && err.meta.body && err.meta.body.status ? err.meta.body.status : httpStatus.INTERNAL_SERVER_ERROR;
+      const statusCode =
+        err && err.meta && err.meta.body && err.meta.body.status
+          ? err.meta.body.status
+          : httpStatus.INTERNAL_SERVER_ERROR;
       return res.status(statusCode).json(err);
     }
   }
