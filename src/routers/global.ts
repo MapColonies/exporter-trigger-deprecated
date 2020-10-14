a585402b968e6dda672bd6ea0de880cba1f7729c
Asaf <asaf.masa@gmail.com>
2020-10-14 09:27:22 +0300

    fix(execution status): change path for getting execution status (#44)
    Co-authored-by: shimoncohen <shimoncohen655@gmail.com>

--- src/routers/global.ts
+++ src/routers/global.ts
@@ -6,6 +6,6 @@ import { swaggerRouter } from './swagger';
 const globalRouter = Router();
 globalRouter.use(swaggerRouter);
 globalRouter.use('/exportGeopackage', exportGeopackageRouter);
-globalRouter.use('/status', getExecutionStatusRouter);
+globalRouter.use('/exportStatus', getExecutionStatusRouter);
 
 export { globalRouter };
