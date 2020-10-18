1393fad3ddfd1955b91a5976a9a8bfa6800f2384
shimoncohen <33935191+shimoncohen@users.noreply.github.com>
2020-10-18 11:03:22 +0300

    feat(status response): add bbox to export status response (#46)

--- src/model/exportStatusRequest.ts
+++ src/model/exportStatusRequest.ts
@@ -5,6 +5,16 @@ interface IExportStatusResponse {
   fileName: string;
   sizeEst: number;
   tilesEst: number;
+  bbox: {
+    topRight: {
+      lat: number;
+      lon: number;
+    };
+    bottomLeft: {
+      lat: number;
+      lon: number;
+    };
+  };
   status: string;
   link: string;
   creationDate: string;
@@ -22,6 +32,16 @@ export function createStatusResponseBody(
     fileName: exportData.fileName,
     sizeEst: exportData.sizeEst,
     tilesEst: exportData.tilesEst,
+    bbox: {
+      topRight: {
+        lat: exportData.bbox[3],
+        lon: exportData.bbox[2],
+      },
+      bottomLeft: {
+        lat: exportData.bbox[1],
+        lon: exportData.bbox[0],
+      },
+    },
     status: 'pending',
     link: '',
     creationDate: currentDate,
