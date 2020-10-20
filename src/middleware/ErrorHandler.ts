e3576840821cf1e7a3a25883a07c9c4806f2be17
shimoncohen <shimoncohen655@gmail.com>
2020-10-20 19:01:38 +0300

    chore(global handler): add check for errors from openapi, and return only error name

--- src/middleware/ErrorHandler.ts
+++ src/middleware/ErrorHandler.ts
@@ -2,6 +2,7 @@ import { MCLogger } from '@map-colonies/mc-logger';
 import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
 import { injectable } from 'tsyringe';
 import { StatusCodes } from 'http-status-codes';
+import { InputValidationError } from 'openapi-validator-middleware';
 
 @injectable()
 export class ErrorHandler {
@@ -18,7 +19,19 @@ export class ErrorHandler {
       this.logger.error(
         `${req.method} request to ${req.originalUrl}  has failed with error: ${err.message}`
       );
-      res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+
+      if (err instanceof InputValidationError) {
+        res.status(StatusCodes.BAD_REQUEST).json({
+          statusCode: StatusCodes.BAD_REQUEST,
+          message: {
+            validationErrors: err.errors,
+          },
+        });
+      } else {
+        res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .send(`Error: ${err.name}`);
+      }
     };
   }
 }
