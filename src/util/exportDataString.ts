c7a4435b4e00eadb5f0a33b50ea36555aecbd703
shimoncohen <shimoncohen655@gmail.com>
2020-10-20 18:57:59 +0300

    chore: add utilities

---
+++ src/util/exportDataString.ts
@@ -0,0 +1,21 @@
+import { IExportData, IInboundRequest } from '../model/exportRequest';
+import { BadRequestError } from '../requests/errors/errors';
+
+export default function (
+  taskId: string,
+  request: IInboundRequest
+): IExportData {
+  try {
+    const exportData: IExportData = {
+      taskId: taskId,
+      fileName: request.fileName,
+      sizeEst: request.sizeEst,
+      tilesEst: request.tilesEst,
+      bbox: request.bbox,
+    };
+
+    return exportData;
+  } catch (error) {
+    throw new BadRequestError(error);
+  }
+}
