2edfca305d60b937dd3e8030add94c2dbcff8340
shimoncohen <shimoncohen655@gmail.com>
2020-10-20 19:04:36 +0300

    chore(export controller): clean controller from logic

--- src/controllers/exportGeopackage.ts
+++ src/controllers/exportGeopackage.ts
@@ -1,12 +1,15 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status-codes';
 import { delay, inject, injectable } from 'tsyringe';
 import { v4 as uuidv4 } from 'uuid';
+import { get } from 'config';
 import { KafkaManager } from '../kafka/manager';
 import { CommonStorageManager } from '../commonStorage/commonStorageManager';
-import { IInboundRequest, IOutboundRequest } from '../model/exportRequest';
-import { get } from 'config';
+import { IInboundRequest } from '../model/exportRequest';
 import { ICommonStorageConfig } from '../model/commonStorageConfig';
+import { HttpError } from '../requests/errors/errors';
+import outboundRequestString from '../util/outboundRequestToExport';
+import exportDataString from '../util/exportDataString';
 
 @injectable()
 export class ExportGeopackageController {
@@ -23,44 +26,23 @@ export class ExportGeopackageController {
 
   public async exportRequestHandler(
     req: Request,
-    res: Response
-  ): Promise<Response> {
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> {
     const requestBody = req.body as IInboundRequest;
     const taskId = uuidv4();
-    let messageToSend: string;
-    try {
-      const parsedMessage: IOutboundRequest = {
-        taskId: taskId,
-        fileName: requestBody.fileName,
-        url: requestBody.exportedLayers[0].url,
-        bbox: requestBody.bbox,
-      };
-      messageToSend = JSON.stringify(parsedMessage);
-    } catch (err) {
-      return res.status(httpStatus.BAD_REQUEST).json(err);
-    }
 
     try {
+      const messageToSend = outboundRequestString(taskId, requestBody);
       await this.kafkaManager.sendMessage(messageToSend);
-    } catch (err) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
-      const statusCode =
-        err && err.meta && err.meta.body && err.meta.body.status
-          ? err.meta.body.status
-          : httpStatus.INTERNAL_SERVER_ERROR;
-      return res.status(statusCode).json(err);
-    }
-
-    try {
-      await this.commonStorageManager.saveExportData({
-        taskId: taskId,
-        fileName: requestBody.fileName,
-        sizeEst: requestBody.sizeEst,
-        tilesEst: requestBody.tilesEst,
-        bbox: requestBody.bbox,
-      });
-    } catch (err) {
-      return res.status(httpStatus.INTERNAL_SERVER_ERROR).json(err);
+      await this.commonStorageManager.saveExportData(
+        exportDataString(taskId, requestBody)
+      );
+    } catch (error) {
+      if (error instanceof HttpError) {
+        return res.status(error.status).json(error);
+      }
+      return next(error);
     }
 
     return res.status(httpStatus.OK).json({ uuid: taskId });
