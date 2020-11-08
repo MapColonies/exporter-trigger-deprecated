c2f1d5e75e1f147bb08ab791d90c65fe61e0e9cd
shimoncohen <33935191+shimoncohen@users.noreply.github.com>
2020-11-08 10:44:56 +0200

    feat(expiration time): add expiration time to export data (#80)

--- src/util/requestToStatus.ts
+++ src/util/requestToStatus.ts
@@ -1,22 +1,27 @@
+import { get } from 'config';
+import { ICommonStorageConfig } from '../model/commonStorageConfig';
 import { IExportData } from '../model/exportRequest';
 import { IExportStatusData } from '../model/exportStatus';
 
 export default function (exportData: IExportData): IExportStatusData {
+  const config: ICommonStorageConfig = get('commonStorage');
   const currentDate = new Date(new Date().toUTCString());
+  const expirationTime = new Date(currentDate);
+  expirationTime.setDate(expirationTime.getDate() + config.expirationTime);
   const userId = 'tester';
   return {
     taskId: exportData.taskId,
     userId: userId,
-    fileURI: exportData.directoryName,
-    fileName: exportData.fileName,
+    fileURI: '',
     directoryName: exportData.directoryName,
+    fileName: exportData.fileName,
     estimatedFileSize: exportData.sizeEst,
     realFileSize: 0,
     tilesEst: exportData.tilesEst,
     geometry: exportData.polygon,
-    status: 'pending',
+    status: 'Pending',
     creationTime: currentDate,
-    expirationTime: currentDate, // todo: set expiration
+    expirationTime: expirationTime,
     updatedTime: currentDate,
     progress: 0,
   };
