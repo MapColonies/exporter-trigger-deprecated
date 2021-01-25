1cd5bdde3cfce56600850791c2a57fa9526960e2
Anguiar <shacharroy14@gmail.com>
2021-01-25 15:41:58 +0200

    feat: add ssl support to kafka configuration

--- src/model/kafkaConfig.ts
+++ src/model/kafkaConfig.ts
@@ -1,5 +1,13 @@
+export interface IKafkaSSLOptions {
+  rejectUnauthorized: boolean;
+  ca: string;
+  key: string;
+  cert: string;
+}
+
 export interface IKafkaConfig {
   clientId: string;
   brokers: string[];
   topic: string;
+  ssl: IKafkaSSLOptions;
 }
