f4b924ceeaa9583ebcd2ef9b5021f83ab9dbd0b5
Anguiar <shacharroy14@gmail.com>
2021-08-03 17:55:15 +0300

    fix: lint errors

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -1,3 +1,5 @@
+// TODO: Will be replaced by MC-UTILS
 export const zoomLevelFromRes = (resolution: number): number => {
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
     return Math.floor(Math.log2(180 / (resolution * 256)));
 }
