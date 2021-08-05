037f759e886248928a814183f6746d02ac1dbcc4
Anguiar <shacharroy14@gmail.com>
2021-08-05 14:16:33 +0300

    fix: lint error

--- src/controllers/gmController.ts
+++ src/controllers/gmController.ts
@@ -45,7 +45,7 @@ export class GMController {
             `${this.rasterCatalogManagerConfig.url}/records/find`,
             { id: req.body.dbId }
           )
-        ).data as Record<string, unknown>[]
+        ).data as (Record<string, unknown> | undefined)[]
       )[0];
 
       if (layer === undefined) {
